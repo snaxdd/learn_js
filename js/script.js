3c1843f52feefd16f65efffd25761ddaa60498b4
Nikita Shabulin <nwpress@yandex.ru>
2020-06-04 15:39:21 +0700

    Добавлен .jshintrc, переименованы папки стилей и скриптов

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+
+alert('Hello World!');
+
+console.log('Lesson 1: Введение в язык, подключение javascript. Основы работы с Git');
