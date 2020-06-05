0ff330cbfd335f4b249ec94d6038b08a7d2af074
Nikita Shabulin <nwpress@yandex.ru>
2020-06-05 13:50:15 +0700

    Изменен файл js и index.html под 2-й урок

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 65535,
+    income = "Фриланс",
+    addExpenses = "Интернет, Steam, Такси",
+    deposit = true,
+    mission = 1000000,
+    period = 11,
+    budgetDay = money / 30;
 
-alert('Hello World!');
-
-console.log('Lesson 1: Введение в язык, подключение javascript. Основы работы с Git');
+console.log( "Money: " + typeof money );
+console.log( "Income: " + typeof income );
+console.log( "Deposit: " + typeof deposit );
+console.log( "Длина строки addExpenses = " + addExpenses.length + " символа" );
+console.log( "Период равен " + period + " месяцев" );
+console.log( "Цель заработать " + mission + " рублей" );
+console.log( addExpenses.toLowerCase().split(", ") );
+console.log( "Бюджет на 1 день = " + budgetDay + " рублей" );
