fc4a19d3045be13e2e11415f60640b190698933b
Nikita Shabulin <nwpress@yandex.ru>
2020-06-09 16:37:46 +0700

    Сделано 4 усложненное упражнение

--- js/script.js
+++ js/script.js
@@ -1,57 +1,16 @@
 "use strict";
 
-function getExpensesMonth(expenses1, expenses2) {
-    return expenses1 + expenses2;
-}
+let myStr = "   Lorem ipsum dolor sit amet, consect   "; //35 символов. 3 пробела вконце и начале.
 
-function getAccumulatedMonth (income, expenses1, expenses2) {
-    return income - (expenses1 + expenses2);
-}
-
-function getTargetMonth(ourMission, ourAccumulatedMonth) {
-    return Math.ceil(ourMission / ourAccumulatedMonth);
-}
-
-function getStatusIncome(ourBudgetDay) {
-    if ( ourBudgetDay >= 1200 ) {
-        return "У вас высокий уровень дохода";
-    } else if ( ourBudgetDay >= 600 && ourBudgetDay < 1200 ) {
-        return "У вас средний уровень дохода";
-    } else if ( ourBudgetDay >= 0 && ourBudgetDay < 600 ) {
-        return "К сожалению у вас уровень дохода ниже среднего";
+function stringBeautify(someString) {
+    if ( typeof someString !== "string" ) {       
+        return alert(`Был передан тип - ${ typeof someString }`);
+    } else if ( someString.trim().length > 30 ) {
+        return someString.trim().slice(0, 29) + "...";
     } else {
-        return "Что-то пошло не так";
-    }
+        return someString.trim();
+    } 
 }
 
-const showTypeOf = function(data) {
-    let result = data + " - " + typeof data;
-    return result;
-};
-
-let money = Number( prompt( "Ваш месячный доход?", 44500 ) ),
-    income = "Фриланс",
-    addExpenses = prompt( "Перечислите возможные расходы за рассчитываемый период через запятую", 
-        "Такси, кафе, интернет" ),
-    deposit = confirm( "Есть ли у вас депозит в банке?" ),
-    mission = 1000000,
-    period = 11,
-    expenses1 = prompt( "Введите обязательную статью расходов", "Steam" ),
-    amount1 = Number( prompt( "Во сколько это обойдется?", "5000" ) ), 
-    expenses2 = prompt( "Введите обязательную статью расходов", "Автомойка" ),
-    amount2 = Number( prompt( "Во сколько это обойдется?", "3500" ) ),
-    accumulatedMonth = getAccumulatedMonth(money, amount1, amount2),
-    budgetDay = accumulatedMonth / 30;
-
-console.log( showTypeOf(money) );
-console.log( showTypeOf(income) );
-console.log( showTypeOf(deposit) );
-
-console.log( `Расходы за месяц = ${ getExpensesMonth(amount1, amount2) }` );
-console.log( addExpenses.toLowerCase().split(", ") );
-console.log( `Цель будет достигнута через ${ getTargetMonth(mission, accumulatedMonth) } месяцев` );
-console.log( `Бюджет на 1 день = ${ budgetDay } руб.` );
-console.log( getStatusIncome(budgetDay) );
-
-
-
+console.log( stringBeautify(myStr) );
+//Lorem ipsum dolor sit amet, c...
