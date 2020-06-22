2ac7a7c27b1fa685587b69f54c1a226dded03471
Nikita Shabulin <nwpress@yandex.ru>
2020-06-23 06:03:53 +0700

    завершен бонусный урок

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 "use strict";
 
+let color = document.querySelector(".wrapper span"),
+    button = document.querySelector("#generate"),
+    buttonAuto = document.querySelector("#generate-auto"),
+    body = document.querySelector("body");
+
+const randomNumber = function(max) {
+    return Math.floor(Math.random() * max);
+};
+
+const generateRandomColor = function() {
+    const red = randomNumber(256).toString(16),
+        green = randomNumber(256).toString(16),
+        blue = randomNumber(256).toString(16);
+
+    return "#" + red + green + blue;    
+};
+
+button.addEventListener("click", function() {
+    color.innerHTML = generateRandomColor();
+    body.style = "background-color: " + generateRandomColor();
+});
+
+buttonAuto.addEventListener("click", function() {
+    setInterval(function() {
+        color.innerHTML = generateRandomColor();
+        body.style = "background-color: " + generateRandomColor();
+    }, 1000);
+});
