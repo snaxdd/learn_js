0e5da5be8045a40a7efbcc8162a6e0eb0ca0cb03
Nikita Shabulin <nwpress@yandex.ru>
2020-06-25 02:53:21 +0700

    Исправлен код

--- js/script.js
+++ js/script.js
@@ -1,41 +1,45 @@
 "use strict";
 
-const DomElement = function() {
-    this.selector = "";
-    this.height = "";
-    this.width = "";
-    this.bg = "";
-    this.fontSize = "";
+const DomElement = function (
+    selector = "#element",
+    height = "100px",
+    width = "100px",
+    bg = "#7EB3FF",
+    fontSize = "16px",
+    text = "Hello!") {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.text = text;
 };
 
-DomElement.prototype.createElement = function(parent) {
-    this.newElem = document.createElement("div");
+DomElement.prototype.createElement = function (parent = "body") {
+    let elemClass = this.selector === "#" ? "p" : "div";
+
+    this.newElem = document.createElement(elemClass);
     this.styleString = "height: " + this.height + "; width: " + this.width + "; background: " + this.bg +
     "; font-size: " + this.fontSize + ";";
 
     if (this.selector[0] === ".") {
-        this.newElem.setAttribute("class", this.selector.slice(1, this.selector.length));    
+        this.newElem.setAttribute("class", this.selector.slice(1, this.selector.length));
     } else if (this.selector[0] === "#") {
         this.newElem.setAttribute("id", this.selector.slice(1, this.selector.length));
     }
 
     this.newElem.setAttribute("style", this.styleString);
+    this.newElem.innerText = this.text;
     document.querySelector(parent).append(this.newElem);
 };
 
-DomElement.prototype.addText = function(text, color) {
-    this.newElem.innerText = text;
-    this.styleString += " color: " + color + ";";
+DomElement.prototype.addStyle = function (style) {
+    this.styleString += style;
     this.newElem.setAttribute("style", this.styleString);
 };
 
-const newElement = new DomElement();
+const newElement = new DomElement(".element");
+const newElement2 = new DomElement("#element", "500px", "500px", "#f9f9f9", "12px", "Bye!");
 
-newElement.selector = "#elem";
-newElement.height = "100px";
-newElement.width = "300px";
-newElement.bg = "#000";
-newElement.fontSize = "32px";
-
-newElement.createElement("body");
-newElement.addText("Новый текст", "white");
+newElement2.createElement();
+newElement.createElement("#element");
